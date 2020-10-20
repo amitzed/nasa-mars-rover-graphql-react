a5c807b49300df3a7b9ace6fe4473137d90ec4b9
amitzed <amitzmn@gmail.com>
2020-10-20 17:28:19 -0400

    INstalled apollo-boost apollo-react-hooks graphql and bootstrap cdn in react client

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        App
+      </div>
+    );
+  }
+}
+
+export default App;
