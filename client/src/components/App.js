dae85bdf5ea3cd723fbf46057549e47e9494f0d2
amitzed <amitzmn@gmail.com>
2020-10-20 18:58:42 -0400

    Data now successfully displaying on front end

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,64 @@
 import React from 'react';
 
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        App
-      </div>
-    );
+const GET_PHOTOS = gql`
+  {
+    photos {
+      id
+      camera {
+        full_name
+        rover_id
+      }
+      img_src
+      rover {
+        name
+        status
+      }
+    }
   }
+`;
+
+const Photo = ({ photo: { camera: {full_name, rover_id}, img_src, rover: {name, status} } }) => (
+  <div>
+    <h2><em>Camera:&nbsp;</em>{full_name}</h2>
+    <h5>Rover Name: {name}</h5>
+    <img src={img_src} alt="Pic" />
+  </div>
+)
+
+const App = () => {
+  const { data, loading, error } = useQuery(GET_PHOTOS);
+
+  if (loading) return (
+    <div className="container app-wrapper">
+      <div className="jumbotron">
+        <h1 className="display-4">Loading...</h1>
+      </div>
+    </div>
+  )
+
+  if (error) return (
+    <div className="container app-wrapper">
+      <div className="jumbotron">
+        <h1 className="display-4">Something Went Wrong...</h1>
+      </div>
+    </div>
+  )
+
+  return (
+    <div className="container app-wrapper">
+      <div className="jumbotron">
+        <h1 className="display-4">NASA Curiosity Mars Rover Images</h1>
+      </div>
+      {data.photos.map(photo => (
+        <Photo key={photo.id} photo={photo} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
 import './index.css';
 import App from './components/App';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
+
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.querySelector('#root')
 )
